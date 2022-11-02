82ea887fe24fa2e577a2db20f572ee234e42c184
infralise123984 <onlytomocorreo@gmail.com>
2022-11-01 21:51:05 -0300

    sistema de las publicaciones que no tiene nada

--- InteriorBotones/publicaciones.php
+++ InteriorBotones/publicaciones.php
@@ -1,1 +1,31 @@
-<!-- prueba -->
+<?php
+session_start();
+if(empty($_SESSION)){
+    header("location: login.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UFT-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,700&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/fontello.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/estilos.css">
+    <link rel="shortcut icon" href="../img/logo.png">
+    <title>iHelp</title>
+</head>
+<body>
+        <!-- contenido principal -->
+        <div class="mx-auto d-flex m-lg-auto justify-content-between"> 
+        aqui deben ir las publicaciones
+        <!-- div cerrador del contenido principal -->
+        </div>         
+    </main>
+   <script src="js/jquery-3.6.1.min.js"></script>
+   <script src="js/menu.js"></script>
+   <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
