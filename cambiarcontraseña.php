88944e85639d5c7b107cfbaf164a8de719d1f1b0
infralise123984 <onlytomocorreo@gmail.com>
2022-11-12 11:08:54 -0300

    cambiar contraseña "funcional"

---
+++ cambiarcontraseña.php
@@ -0,0 +1,81 @@
+<?php 
+    session_start();
+    if(empty($_SESSION)){
+    header("location: login.php");
+}
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,700&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/fontello.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="shortcut icon" href="img/logo.png">
+    <title>iHelp</title>
+</head>
+
+<body>
+    <div class="container w-75 bg-primary mt-5 rounded shadow">
+        <div class="row align-items-stretch">
+            <div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-lg-5 col-xl-6 rounded">
+                
+            </div>
+            <div class="col bg-white p-5 rounded-end">
+                <div class="text-end">
+                    <img src="img/logo.png" width="48" alt="">
+                </div>
+
+                <h2 class="fw-bold text-center py-5">Cambiar contraseña</h2>
+
+                <!-- cambio de contra UWU -->
+                <?php 
+                    include "modelo/conexion.php";
+                    include "controlador/controlador_cambiar_contraseña.php";
+                ?>
+                <!-- Formulario -->
+                <form action="" method=post oninput='newpassword2.setCustomValidity(newpassword2.value != newpassword.value ? "Las contraseñas no coinciden." : "")'>  
+                    <div class="mb-4">
+                        <!-- contraseña actual formulario oculta con formato . 8 caracteres minimo, una mayuscula y almenos un numero -->
+                        <label for="password" class="form-label">Contraseña actual</label>
+                        <input type="password" class="form-control" name="actualpassword" placeholder="Ejemplo: Contrasena4" id="pswrd" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Almenos 1 mayuscula, minimo 8 caracteres y un numero"> 
+                    </div>        
+                    <div class="mb-4">
+                        <!-- contraseña nueva formulario oculta con formato . 8 caracteres minimo, una mayuscula y almenos un numero -->
+                        <label for="password" class="form-label">Nueva contraseña</label>
+                        <input type="password" class="form-control" name="newpassword" placeholder="Ejemplo: Contrasena42" id="pswrd" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Almenos 1 mayuscula, minimo 8 caracteres y un numero"> 
+                    </div>
+                    <div class="mb-4">
+                        <!-- confirmacion de contraseña funcional -->
+                        <label for="password" class="form-label">Confirmar  nueva contraseña</label>
+                        <input type="password" class="form-control" name="newpassword2" placeholder="Ejemplo: Contrasena42" id="pswrd" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}">
+                        <input type="checkbox" onclick="toggleVisibility()"/>Mostrar Contraseña
+                    </div>
+                    <!-- boton para cambiar la contraseña -->
+                    <div class="d-grid">
+                    <input name="btncambiar" class="btn btn-primary" type="submit" value="cambiar">
+                    </div>
+                    <!-- añadir tabla en la cual hayan 2 opciones, 1.no trabajor o 2.Trabajador -->
+                    <div class="my-3">
+                        <span><br><a href="login.php">Volver</a></span>    
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+    <script src="js/jquery-3.6.1.min.js"></script>
+    <script src="js/menu.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/pass-show-hide.js"></script>
+ </body>
+ </html>
--- controlador/controlador_cambiar_contraseña.php
+++ controlador/controlador_cambiar_contraseña.php
@@ -1,38 +1,59 @@
-<?php
-session_start();
-if(empty($_SESSION)){
-    header("location: login.php");
-}
-     // en resumen no funciona 
-        
-            //rescato los datos de los div
-            $passactual = $mysqli->real_escape_string($_POST['passactual']);
-            $password = $mysqli->real_escape_string($_POST['password']);
-            $password2 = $mysqli->real_escape_string($_POST['password2']);
+<?php   
+            //este if para cuando se apreta el btoboton
+            if(!empty($_POST["btncambiar"])){
+                //si los recuadros de las contraseñas no están vacios, va a ejecutar el codigo dentro
+                if(!empty ($_POST["actualpassword"]) and !empty ($_POST["newpassword"]) and !empty ($_POST["newpassword2"])){
+                    //aqui se guardan en variables los recuadros de las contraseñas
+                    $passactual = $_POST['actualpassword'];
+                    $password = $_POST['newpassword'];
+                    $password2 = $_POST['newpassword2'];
+                    $rut=$_SESSION['rut'];
+                    $sqlA=$conexion->query("SELECT contraseña  FROM usuario WHERE rut = '$rut'");
+                    $datos1=$sqlA->fetch_object();
+                    $passDB=$datos1->contraseña;
+                    if($password    !=$passDB){
+                        //si la contraseña es distinta a contraseña dentro de la base de datos, va a ejecutar este codigo
+                        //echo 'las contraseñas son distintas';
+                        $update = $conexion->query("UPDATE usuario SET contraseña ='$password'WHERE rut='$rut' ");
+                        echo '<script language="javascript">alert("Contraseña cambiada exitosamente");</script>';  
+                        header("location: index.php");
+                        
+
+                    }else{
+                        //si la contraseña es igual a la de la base de datos, se va a ejecutar este codigo
+                        //echo 'las contraseñas son iguales';
+                        echo '<script language="javascript">alert("porfavor usar una contraseña distinta a su contraseña actual");</script>';     
+                    }
+
+                }else{
+                    //si los campos están vacios, se va a ejecutar este codigo
+                    echo '<script language="javascript">alert("porfavor rellenar todos los campos");</script>';     
+                }               
+            }
             //El md5 es para enciptat la contraseña en la base de datos (puede que esto sea posible error para cambiar la contraseña)
-            $passactual = md5 ($passactual);
-            $password = md5 ($password);
-            $password2 = md5 ($password2);
+            // $passactual = md5 ($passactual);
+            // $password = md5 ($password);
+            // $password2 = md5 ($password2);
 
             //consulta la contraseña del usuraio que donde el rut sea igual al rut que entro a session
-            $sqlA=$mysqli->query("SELECT contraseña FROM usuario WHERE rut ='".$_SESSION['rut']."' ");
-            $rowA=$sqlA->fecht_array();
-            if($rowA['contraseña']==$passactual){
+            // $sqlA=$mysqli->query("SELECT contraseña FROM usuario WHERE rut ='".$_SESSION['rut']."' ");
+            // $rowA=$sqlA->fecht_array();
+            // if($rowA['contraseña']==$passactual){
 
-                if ($password==$password2) {
-                    // consulta para cambiar contraseña
-                    $update = $mysqli->query("UPDATE usuario SET contraseña ='$password'WHERE rut='".$_SESSION['rut']."' ");
-                    if($update){echo "se actualizo tucontraseña";}
-                }
-                else{
-                    echo"las dos contraseña no coinciden";
-                }
+            //     if ($password==$password2) {
+            //         // consulta para cambiar contraseña
+            //         $update = $mysqli->query("UPDATE usuario SET contraseña ='$password'WHERE rut='".$_SESSION['rut']."' ");
+            //         if($update){echo "se actualizo tucontraseña";}
+            //     }
+            //     else{
+            //         echo"las dos contraseña no coinciden";
+            //     }
 
-            }else{
-                echo"no coincide tu contraseña actual";
-            }
+            // }else{
+            //     echo"no coincide tu contraseña actual";
+            // }
             
-        }
+        
 
 ?>
 
