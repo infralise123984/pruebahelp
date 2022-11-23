0076d83e0f8c1bb62aaee5c7156717f5aa62cc8d
Rxzion <7x.alejandr@gmail.com>
2022-11-23 15:42:47 -0300

    cambio chico 2 a

--- controlador/controlador_subirfoto_publicacion.php
+++ controlador/controlador_subirfoto_publicacion.php
@@ -13,7 +13,7 @@ if (isset($_POST['btncrearpubli']) && isset($_FILES['imagen'])) {
 				$img_upload_path = '../img/'.$new_img_name;
 				move_uploaded_file($tmp_name, $img_upload_path);
 	 			$sql=$conexion->query("INSERT INTO publicacion(titulo,info,creador,estado,imagen) VALUES ('$titulo','$desc','$_SESSION[unique_id]','1','$new_img_name')");
-	 			mysqli_query($conexion, $sql);
+	 			
 				 echo '<script language="javascript">alert("se subio con foto");</script>';  
 				
 		}
@@ -21,7 +21,7 @@ if (isset($_POST['btncrearpubli']) && isset($_FILES['imagen'])) {
 			$titulo=$_POST['titulo'];
 			$desc=$_POST['comentario'];
 			$sql=$conexion->query("INSERT INTO publicacion(titulo,info,creador,estado) VALUES ('$titulo','$desc','$_SESSION[unique_id]','1')");
-			mysqli_query($conexion, $sql);
+			
 			echo '<script language="javascript">alert("se subio sin foto");</script>'; 
 
 		}
