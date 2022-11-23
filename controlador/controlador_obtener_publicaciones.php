ecd48d525298b1dd47dcd04e7b1a4c1a00486be2
ASYCK <ma.muestudios@gmail.com>
2022-11-23 16:07:38 -0300

    a

--- controlador/controlador_obtener_publicaciones.php
+++ controlador/controlador_obtener_publicaciones.php
@@ -3,7 +3,7 @@ $sql=$conexion->query("SELECT * FROM publicacion");
 $output ="";
 $row = mysqli_fetch_assoc($sql);
 if(mysqli_num_rows($sql) == 0){
-    $output .= "No hay publicaciones disponibles";
+    $output = "No hay publicaciones disponibles";
 }elseif(mysqli_num_rows($sql) > 0){
     $sql2=$conexion->query("SELECT * FROM publicacion ORDER BY fecha desc");
     //$output .= "si hay cosas";
